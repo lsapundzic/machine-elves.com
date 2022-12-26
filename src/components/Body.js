20f1e99d53d720cca885309e64a75b42633eb31a
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-12-26 16:04:59 +0100

    Prep for api call

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,12 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Body = () => {
+  const [note, newNote] = useState([]);
+
   return (
     <div>
-      <div>Content</div>
+      <div>No content yet</div>
     </div>
   );
 };
