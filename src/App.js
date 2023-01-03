b300ae7e733efffff93ca384ad8bcd2c0a22652a
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-01-03 14:50:32 +0100

    Major pages of the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./styles/App.css";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
-import Body from "./components/Body";
 import Footer from "./components/Footer";
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <div>
       <Header />
       <Navbar />
-      <Body />
       <Footer />
     </div>
   );
