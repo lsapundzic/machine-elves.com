d14ab618772aeac6390772f09bec60123c504b7e
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-01-04 16:33:35 +0100

    Poor attempt at navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,17 @@
 const Navbar = () => {
-  return <nav className="navbar"></nav>;
+  return <nav className="navbar">
+      <ul>
+          <li>
+              <a href="./pages/About.js">About</a>
+          </li>
+          <li>
+              <a href="./pages/Gallery.js">Gallery</a>
+          </li>
+          <li>
+              <a href="./pages/Contact.js">Contact</a>
+          </li>
+      </ul>
+  </nav>;
 };
 
 export default Navbar;
