921e5901481a3bf6b5f0734a332a98ecf713f8c8
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-06-08 13:56:12 +0200

    Under construction

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 export default function Home() {
   return (
     <main>
-      <h1>Machine Elves</h1>
-      <h2>Psychedelic Art & Blog</h2>
-
-      <p>The page is currently in production. Please come back later.</p>
-
-      <p>Website by CashmanSOL</p>
+      <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content text-center">
+    <div className="max-w-md">
+      <h1 className="text-5xl font-bold">Machine Elves Psychedelic Artwork</h1>
+      <p className="py-6">This page is currently under construction. We are looking forward to your visit in the near future</p>
+      <button className="btn btn-primary">Press Me :3</button>
+    </div>
+  </div>
+</div>
     </main>
   )
 }
