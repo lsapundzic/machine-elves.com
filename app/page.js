85e6e4617bbbf7d6f3830ee327ab4e4ce76eee4c
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-10-10 21:24:48 +0200

    Basic message added

--- app/page.js
+++ app/page.js
@@ -2,6 +2,11 @@ import Image from 'next/image'
 
 export default function Home() {
     return (
-        <h1>Welcome</h1>
+        <>
+            <h1>Machine Elves</h1>
+            <h2>Curated Psychedelic Art Gallery</h2>
+            <p>This site is currently in development. Please come back later. Thanks :)</p>
+        </>
+
     )
 }
