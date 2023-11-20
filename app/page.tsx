eabee274483378c3799e58ad94979bafb7b3d67a
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-11-20 22:15:02 +0100

    template realized

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
     return (
         <div>
             <div className="flex flex-col items-center justify-center min-h-screen">
-                <Image width={100} height={300} className="mb-4" src="/public/judge.jpg" alt="An art deco style head statue" />
-                <p className="text-lg text-gray-600">Machine Elves</p>
+                <Image width={460} height={460} className="mb-4" src="/images/judge.jpg" alt="An art deco style head statue" />
+                <p className="text-lg">Machine Elves</p>
             </div>
         </div>
     )
