e6fae0f8285fb149b41ed4441f913f5042409125
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-11-20 22:45:59 +0100

    resolving variable image cycle

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
-import Image from 'next/image'
+"use client";
+
+import Image from "next/image";
 
 export default function Home() {
-    return (
-        <div>
-            <div className="flex flex-col items-center justify-center min-h-screen">
-                <Image width={460} height={460} className="mb-4" src="/images/judge.jpg" alt="An art deco style head statue" />
-                <p className="text-lg">Machine Elves</p>
-            </div>
-        </div>
-    )
+  const path = "/images/";
+  let images = [`${path}judge.jpg`, `${path}god.jpg`, `${path}witness.jpg`];
+
+  const catchClick = () => {
+    alert("You clicked me");
+  };
+
+  return (
+    <div>
+      <div
+        className="flex flex-col items-center justify-center min-h-screen"
+        onClick={catchClick}
+      >
+        <Image
+          width={460}
+          height={460}
+          className="mb-4"
+          src={images[2]}
+          alt="An art deco style head statue"
+        />
+        <p className="text-5xl">Machine Elves</p>
+      </div>
+    </div>
+  );
 }
