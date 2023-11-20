ce805d14faf11e9b3281565f8dfb3c9d15cfa777
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-11-20 21:49:51 +0100

    enabled the test page

---
+++ app/tests/test.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+const Test = () => {
+    return (
+        <div>
+            <h1>You have reached the page for project testing</h1>
+            <h2>Please return home immediately</h2>
+            <Link href="./"></Link>
+        </div>
+    )
+}
