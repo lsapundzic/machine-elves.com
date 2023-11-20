970694e0234c1f2b4140fa4736e1deb2096522a8
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-11-20 22:03:51 +0100

    adding image to the center of the screen

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import Image from 'next/image'
 export default function Home() {
     return (
         <div>
-            <h1>Machine Elves</h1>
+            <div className="flex flex-col items-center justify-center min-h-screen">
+                <Image width={100} height={300} className="mb-4" src="/public/judge.jpg" alt="An art deco style head statue" />
+                <p className="text-lg text-gray-600">Machine Elves</p>
+            </div>
         </div>
     )
 }
