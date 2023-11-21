04821f9c140742bd7566aa7a43eed80d87114d89
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-11-21 21:54:03 +0100

    trying a bit of ts

--- app/tests/page.tsx
+++ app/tests/page.tsx
@@ -2,13 +2,25 @@
 
 import Link from "next/link";
 import { name } from "ts-interface-checker";
+import { useState } from "react";
 
 const Test = () => {
+  const [value, setVale] = useState(0);
+
+  const increase = () => {
+    setVale(value + 5);
+  };
+
   return (
     <div>
       <h1>You have reached the page for project testing</h1>
       <h2>Please return home immediately</h2>
       <Link href="./">Take me home</Link>
+      <div>
+        <button className="btn" onClick={increase}>
+          {value}
+        </button>
+      </div>
     </div>
   );
 };
