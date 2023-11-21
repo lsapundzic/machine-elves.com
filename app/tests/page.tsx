a856abfb300beb3f33ebabb2ba7114861cf488f2
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-11-21 21:37:32 +0100

    installed daisy UI

--- app/tests/page.tsx
+++ app/tests/page.tsx
@@ -1,13 +1,16 @@
-import Link from "next/link"
+"use client";
+
+import Link from "next/link";
+import { name } from "ts-interface-checker";
 
 const Test = () => {
-    return (
-        <div>
-            <h1>You have reached the page for project testing</h1>
-            <h2>Please return home immediately</h2>
-            <Link href="./">Take me home</Link>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>You have reached the page for project testing</h1>
+      <h2>Please return home immediately</h2>
+      <Link href="./">Take me home</Link>
+    </div>
+  );
+};
 
-export default Test; 
+export default Test;
