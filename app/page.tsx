a2a1aaec226eaa8b17d7c9c813bb4c32170561f8
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2024-03-03 22:18:03 +0100

    pranky 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 export default function Home() {
     return (
         <main>
-            <h1>Machine Elves</h1>
-            <p>I know who you are, M</p>
-            <sub>Enter the void</sub>
+            <div>
+                <h1>The Machine Elves are watching</h1>
+                <p>Welcome to Earth, creature of Q</p>
+                <br />
+
+                <p>Do what thou wilt shall be the whole of the law.</p>
+            </div>
         </main>
     );
 }
