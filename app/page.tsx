d3258c50995c8befeb6bf6e0c33e84ff0d2980a3
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2024-03-03 22:08:43 +0100

    pranky

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ export default function Home() {
     return (
         <main>
             <h1>Machine Elves</h1>
+            <p>I know who you are, M</p>
+            <sub>Enter the void</sub>
         </main>
     );
 }
